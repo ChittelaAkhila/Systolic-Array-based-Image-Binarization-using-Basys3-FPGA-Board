088702de6a3c2c9c31865768dc94c0e827fdb849
ChittelaAkhila <chittelaakhila@gmail.com>
2025-05-01 15:36:06 +0530

    Update uart_tx.v

--- design sources/uart_tx.v
+++ design sources/uart_tx.v
@@ -1,3 +1,7 @@
+//This module is used for transmitting bits via UART protocol 
+// Refered from the book Pong P. Chu, ”FPGA Prototyping by Verilog Examples: Xilinx Spartan-3
+ // Version”. Hoboken, NJ: Wiley-Interscience, 2008
+
 module uart_tx
 #(
     parameter DBIT = 8,    // Data bits (32 bits)
